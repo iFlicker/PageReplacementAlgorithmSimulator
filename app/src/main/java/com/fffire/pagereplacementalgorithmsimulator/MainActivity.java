b85e5c5e02fb78a12994b999bf0a376e625292a4
iFlicker <ffflicker@gmail.com>
2015-12-31 19:20:51 +0800

    Initial commit

---
+++ app/src/main/java/com/fffire/pagereplacementalgorithmsimulator/MainActivity.java
@@ -0,0 +1,13 @@
+package com.fffire.pagereplacementalgorithmsimulator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private int b;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
